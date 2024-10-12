00fa0aeeef6a4c85836ed92056eef799bfa8e442
eranda <erandasathsara@gmail.com>
2024-10-12 17:07:44 +0530

    bug fixed

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -4,15 +4,15 @@ import Footer from '../components/Footer';
 import AboutSection from '../components/AboutSection';
 
 const About = () => {
-  return (
-    <div>
-      <Header />
-      <main>
-        <AboutSection />
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow">
+                <AboutSection />
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default About;
