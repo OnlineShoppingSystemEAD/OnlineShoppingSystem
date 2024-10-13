84d2314db5193a24a10c0effffc78d34b4a64d73
eranda <erandasathsara@gmail.com>
2024-10-13 21:57:47 +0530

    home and shop tabs completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './index.css'; // or global.css depending on where you add the styles
 
 function App() {
