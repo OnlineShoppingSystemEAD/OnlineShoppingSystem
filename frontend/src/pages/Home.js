b6019b1d5027e9bfa5769d7d8c93a4ed4e635943
Shivan Tillekeratne <shivanabi123@gmail.com>
2024-10-14 01:22:07 +0530

    removed additional import

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
-import Slider from 'react-slick';
+import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/carouselStyles.css'; // Ensure you import your CSS file here
-import rightArrow from '../images/right-arrow.png';
-import leftArrow from '../images/left-arrow.png';
 import TileGrid from "../components/TileGrid";
 import ProductOverview from "../components/ProductOverview";
 import Carousel from "../components/Carousel";
