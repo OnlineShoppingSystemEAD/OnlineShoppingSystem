cb1f9d1ea7fd57d57fa3d3c1449b840dcada1938
CharukaLakmal98 <charukalakmal920@gmail.com>
2024-10-16 11:26:54 +0530

    contact page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './pages/About';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './index.css'; // or global.css depending on where you add the styles
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./index.css"; // or global.css depending on where you add the styles
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/shop" element={<Shop />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
         {/* Other routes can be added here */}
       </Routes>
     </Router>
