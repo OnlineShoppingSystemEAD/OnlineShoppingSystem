6edcf6ec9218b47e2adbe1fdc581c62d1d809a20
SabAneesah <aneesha13sabar@gmail.com>
2024-10-22 21:49:38 +0530

    -initiate-maven-backend

---
+++ backend/src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package test.java.com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true); // A simple test that will always pass
+    }
+}
