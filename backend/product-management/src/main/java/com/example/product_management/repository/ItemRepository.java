408cdb2c306c8893d980242c100e9c5cb65a438d
Hevindu <95610343+Hevb890@users.noreply.github.com>
2024-10-30 16:17:05 +0530

    Query Update

--- backend/product-management/src/main/java/com/example/product_management/repository/ItemRepository.java
+++ backend/product-management/src/main/java/com/example/product_management/repository/ItemRepository.java
@@ -2,15 +2,18 @@ package com.example.product_management.repository;
 
 import com.example.product_management.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, String> {
-    List<Item> findByCategoryId(String categoryId);
+    @Query(nativeQuery = true, value = "SELECT * FROM Item WHERE categoryId=:categoryId")
+    List<Item> findByCategoryId(@Param("categoryId") String categoryId);
 
     default List<Item> findLimitedItems(int limit) {
         Pageable pageable = PageRequest.of(0, limit);
