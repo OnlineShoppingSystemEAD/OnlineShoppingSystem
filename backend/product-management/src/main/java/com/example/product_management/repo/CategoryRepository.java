4ef6494782f2b0ba1df9411c5fb548e98805f6fb
sharhaan304 <sharhaanmfm.21@uom.lk>
2024-11-20 17:20:09 +0530

    revert try

--- backend/product-management/src/main/java/com/example/product_management/repo/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.product_management.repo;
-
-import com.example.product_management.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
