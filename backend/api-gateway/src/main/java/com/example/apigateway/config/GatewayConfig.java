241f5c2edfdc4dfef7802ef34148696334ccf21e
PERERA WJR <96611644+JanithRavisanka@users.noreply.github.com>
2024-11-20 10:44:23 +0530

    CORS config added

--- backend/api-gateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
+++ backend/api-gateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class GatewayConfig {
@@ -38,5 +41,18 @@ public class GatewayConfig {
                                        .filter(tokenVerificationFilter.apply(new TokenVerificationFilter.Config())))
                         .uri("lb://USER-MANAGEMENT-SERVICE"))
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
