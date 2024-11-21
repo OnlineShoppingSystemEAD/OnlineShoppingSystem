147b6be8d38aad1ba9234ddbbccef483430d3ad2
Hevindu <95610343+Hevb890@users.noreply.github.com>
2024-11-21 23:00:47 +0530

    Update ShoppingCartItemDto.java

--- backend/order-management/src/main/java/com/example/order_management/dto/ShoppingCartItemDto.java
+++ backend/order-management/src/main/java/com/example/order_management/dto/ShoppingCartItemDto.java
@@ -5,15 +5,17 @@ public class ShoppingCartItemDto {
     private String itemName;
     private float itemPrice;
     private int itemQuantity;
+    private String imageURL;
 
     public ShoppingCartItemDto() {
     }
 
-    public ShoppingCartItemDto(int id, String itemName, float itemPrice, int itemQuantity) {
+    public ShoppingCartItemDto(int id, String itemName, float itemPrice, int itemQuantity, String imageURL) {
         this.id = id;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
         this.itemQuantity = itemQuantity;
+        this.imageURL = imageURL;
     }
 
     public ShoppingCartItemDto(int id, int itemQuantity) {
@@ -51,5 +53,13 @@ public class ShoppingCartItemDto {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public void setImageURL(String imageURL){
+        this.imageURL = imageURL;
+    }
+
+    public String getImageURL(){
+        return this.imageURL;
     }
 }
