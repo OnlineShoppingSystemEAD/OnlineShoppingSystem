06ebf9a4cdc5ff3a17d21073ca0ba4d204b6c505
Hevindu <95610343+Hevb890@users.noreply.github.com>
2024-11-23 22:35:16 +0530

    shopping cart repo unit test

---
+++ backend/order-management/src/test/java/com/example/order_management/service/ShoppingCartServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.order_management.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ShoppingCartServiceTest {
+    @Test
+    void testCreateShoppingCartItem() {
+
+    }
+
+    @Test
+    void testDeleteItemFromtheShoppingCart() {
+
+    }
+
+    @Test
+    void testGetShoppingCartByUserId() {
+
+    }
+
+    @Test
+    void testUpdateShoppingCart() {
+
+    }
+}
