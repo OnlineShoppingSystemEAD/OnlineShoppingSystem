d75b59f5850063168bde6e96ac4ea0c1dcd67f22
PERERA WJR <96611644+JanithRavisanka@users.noreply.github.com>
2024-12-02 03:13:23 +0530

    test push 2, workflow updated

--- backend/user-management/src/main/java/com/example/usermanagement/Service/AuthService.java
+++ backend/user-management/src/main/java/com/example/usermanagement/Service/AuthService.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Random;
-
 import java.util.HashMap;
 
 @Service
