4c353a45738a9a3d9b69581e455c566fd31e895a
PERERA WJR <janithravisankax@gmail.com>
2024-12-10 00:54:04 +0530

    just for test ec2

--- backend/user-management/src/main/java/com/example/usermanagement/Service/OurUserDetailsService.java
+++ backend/user-management/src/main/java/com/example/usermanagement/Service/OurUserDetailsService.java
@@ -20,7 +20,6 @@ import java.util.Optional;
 @Service
 public class OurUserDetailsService implements UserDetailsService {
 
-
     private final OurUserRepo ourUserRepo;
     private final UserProfileRepo userProfileRepo;
     private final AmazonS3Service amazonS3Service;
