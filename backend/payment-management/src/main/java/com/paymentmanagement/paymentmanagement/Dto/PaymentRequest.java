7c58dc1d0753599d8e35478a1b69c1e99982ba13
PERERA WJR <janithravisankax@gmail.com>
2024-12-10 20:55:14 +0530

    bug fix payment

--- backend/payment-management/src/main/java/com/paymentmanagement/paymentmanagement/Dto/PaymentRequest.java
+++ backend/payment-management/src/main/java/com/paymentmanagement/paymentmanagement/Dto/PaymentRequest.java
@@ -1,19 +1,21 @@
 package com.paymentmanagement.paymentmanagement.Dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
 public class PaymentRequest {
-    private int userID;
+    private int userId;
     private int orderId;
     private double amount;
 
-    public PaymentRequest(int orderId, double amount, int userID) {
+    public PaymentRequest(int orderId, double amount, int userId) {
         this.orderId = orderId;
         this.amount = amount;
-        this.userID = userID;
+        this.userId = userId;
     }
 
     public PaymentRequest(int i, BigDecimal bigDecimal, String creditCard) {
